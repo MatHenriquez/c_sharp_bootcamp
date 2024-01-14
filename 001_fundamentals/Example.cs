86cdfcc1f5e034c26b0af6c9cd6451762a062cc6
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-14 20:38:08 -0300

    Exemplify Lambda functions

---
+++ 001_fundamentals/Example.cs
@@ -0,0 +1,11 @@
+﻿namespace _001_fundamentals;
+
+internal static class Example
+{
+    public static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static Func<int, int, int> LambdaSum = (a, b) => a + b; // Lambda function. Types: <arg, arg, return>.
+}
---
+++ 001_fundamentals/Program.cs
@@ -0,0 +1,9 @@
+﻿using _001_fundamentals;
+
+int result = Example.Sum(1, 2);
+
+Console.WriteLine($"Sum(1, 2) result is: {result}");
+
+int lambdaResult = Example.LambdaSum(1, 2);
+
+Console.WriteLine($"LambdaSum(1, 2) result is: {result}");
