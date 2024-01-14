3c0e02eca89b2622b276dc0d3e5c865272fa548d
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-14 20:53:58 -0300

    Exemplify regular lambda method

---
+++ 001_csharp_v12/LambdasCases.cs
@@ -0,0 +1,14 @@
+﻿namespace _001_csharp_v12
+{
+    internal class LambdasCases
+    {
+        // Regular Lambda function method.
+        public static int RegularLambdaSum(int x, int y)
+        {
+            Func<int, int, int> LambdaSum = (int x, int y) => x + y;
+            return LambdaSum(x, y);
+        }
+
+
+    }
+}
--- 001_csharp_v12/obj/Debug/net8.0/001_csharp_v12.AssemblyInfo.cs
+++ 001_csharp_v12/obj/Debug/net8.0/001_csharp_v12.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("001_csharp_v12")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86cdfcc1f5e034c26b0af6c9cd6451762a062cc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fce2c99543e04cc3e9cedd8d2d0f342bb58c8a4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("001_csharp_v12")]
 [assembly: System.Reflection.AssemblyTitleAttribute("001_csharp_v12")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
