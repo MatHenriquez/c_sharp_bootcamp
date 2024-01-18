cc11b04597a0ad5958453b3bda362c91ee58a6d2
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-18 19:24:53 -0300

    Implement AddBook method in the BookService class

--- DemoBooks/services/BookService.cs
+++ DemoBooks/services/BookService.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DemoBooks.entitites;
 
 namespace DemoBooks.services
 {
-    public class BookService
+    public static class BookService
     {
+        public static List<Book> books = new List<Book>();
+        public static string AddBook()
+        {
+            Console.WriteLine("Enter the title");
+            string? title = Console.ReadLine();
+
+            Console.WriteLine("Enter the author");
+            string? author = Console.ReadLine();
+
+            Console.WriteLine("Enter the category");
+            string? category = Console.ReadLine();
+
+            Book newBook = new Book 
+            {
+                Id = 1,
+                Title = title,
+                Author = author,
+                Category = category,
+                IsAvailable = true,
+            };
+
+            books.Add(newBook);
+
+            return $"The book {newBook.Title} has been created successfully";
+        }
     }
 }
