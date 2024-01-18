fc258c061ba8e50fe48f25d96310e7b605a4e8fd
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-18 19:09:31 -0300

    Create demo menu

--- DemoBooks/Program.cs
+++ DemoBooks/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to the Bootcamp!");
+Console.WriteLine("1. Create a book");
+Console.WriteLine("2. Edit a book");
+Console.WriteLine("3. Delete a book");
+Console.WriteLine("4. Get all books");
+Console.WriteLine("5. Exit");
+
+string? option = Console.ReadLine();
