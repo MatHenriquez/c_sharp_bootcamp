3f94ef2a06c9fd5cee68f125d1dbfe48ee8f6dbb
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-20 22:25:27 -0300

    Add integer types examples

--- 002_fundamentals_a/Program.cs
+++ 002_fundamentals_a/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class DataType
+{
+    // Integers
+    sbyte sShortNumber = 127;                   // 8 bits, signed (- or +)
+    byte byteNumber = 255;                      // 8 bits, unsigned (0 or +)
+    short shortNumber = 32767;                  // 16 bits, signed
+    ushort uShortNumber = 65535;                // 16 bits, unsigned
+    int intNumber = 2147483647;                 // 32 bits, signed
+    uint uintNumber = 2147483648;               // 32 bits, unsigned
+    long longNumber = 922564415646;             // 64 bits, signed
+    ulong ulongNumber = 9225644156465555555;    // 64 bits, unsigned
+}
