fd7f87de79d0638ba2b5e7fef96a2516916fb899
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-27 15:27:51 -0300

    Exemplify Lambda expression

--- 002_fundamentals_b/Program.cs
+++ 002_fundamentals_b/Program.cs
@@ -1,25 +1,28 @@
 ﻿using _002_fundamentals_b;
 
-//Console.WriteLine(ExceptionsHandler.UnhandledDivision(1, 0)); // Error
-Console.WriteLine(ExceptionsHandler.HandledDivision(1, 0));
+////Console.WriteLine(ExceptionsHandler.UnhandledDivision(1, 0)); // Error
+//Console.WriteLine(ExceptionsHandler.HandledDivision(1, 0));
 
-ExceptionsHandler.Run();
+//ExceptionsHandler.Run();
 
-Console.Clear();
-Console.WriteLine("Press a key to take a nap");
-Console.ReadLine();
+//Console.Clear();
+//Console.WriteLine("Press a key to take a nap");
+//Console.ReadLine();
 
-AsyncExample.TakeNap();
+//AsyncExample.TakeNap();
 
-Console.Clear();
-Console.WriteLine("Press a key to order pizza");
-Console.ReadLine();
+//Console.Clear();
+//Console.WriteLine("Press a key to order pizza");
+//Console.ReadLine();
 
-//AsyncExample.OrderPizza(); // With await this will be similar to sinchronic
-//AsyncExample.TakeShower();
+////AsyncExample.OrderPizza(); // With await this will be similar to sinchronic
+////AsyncExample.TakeShower();
 
-Task orderPizza = AsyncExample.OrderPizza();
-Task takeShower = AsyncExample.TakeShower();
-await Task.WhenAll(orderPizza, takeShower);
+//Task orderPizza = AsyncExample.OrderPizza();
+//Task takeShower = AsyncExample.TakeShower();
+//await Task.WhenAll(orderPizza, takeShower);
 
-Console.ReadKey();
+//Console.ReadKey();
+
+var multiplicar = (int a, int b) => a*b;
+Console.WriteLine((multiplicar(4, 7)));
