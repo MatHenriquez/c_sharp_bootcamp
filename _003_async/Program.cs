3143c3ffd9cc182a9ced51d96696b9eac1b2e885
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-28 17:18:44 -0300

    Exemplify async await Task

--- _003_async/Program.cs
+++ _003_async/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//List<string> pokemons = new List<string>(); Ahora puedo asignar el valor directamente:
+List<string> pokemons = ["Pikachu", "Charmander", "Squirtle"];
+
+Task displayAllPokemons = Task.Run(async () => await GetAll(pokemons)); // Recibe un Action como parámetro
+
+static async Task GetAll(List<string> pokemons)
+{
+    Console.WriteLine("Catching all pokemons");
+
+    await Task.Delay(2000);
+
+    foreach (string pokemon in pokemons)
+        Console.WriteLine(pokemon);
+}
+
+displayAllPokemons.Wait();
+
