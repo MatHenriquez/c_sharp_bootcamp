a40e0ecc78a72aaf03f2f02ed80123e1899d8dcb
MatHenriquez <matias.henriquez.dev@gmail.com>
2024-01-29 08:30:02 -0300

    Add models

---
+++ 004_linq/Models/University.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemos.Models
+{
+    public class University
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
